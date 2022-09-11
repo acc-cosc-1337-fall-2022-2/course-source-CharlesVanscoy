0f6c38e6ec48caca19d78344d9484df5baf72327
CharlesVanscoy <charles.vanscoy@g.austincc.edu>
2022-09-10 22:47:29 -0500

    compiles and runs correctly but CMake issues

--- src/homework/01_data_types/main.cpp
+++ src/homework/01_data_types/main.cpp
@@ -1,18 +1,20 @@
 ﻿//write include statements
-
+#include <iostream>
+#include "data_types.h"
 
 //write namespace using statement for cout
-
+using namespace std;
 
 /*
 Call multiply_numbers with 10 and 10 parameter values and display function result
 */
 
-#include <iostream>
-using namespace std;
-
 int main()
 {
-	cout << "well what";
-	return 0;
+	int product, input1, input2;
+	input1 = 10;
+	input2 = 10;
+	product = multiply_numbers(input1, input2);
+	cout << input1 << " X " << input2 << " = " << product;
+	return product;
 }
