4cd6b7515f907800f6fa93af6dc4c4ef2ac2e2ee
CharlesVanscoy <charles.vanscoy@g.austincc.edu>
2022-09-18 00:18:43 -0500

    Works Betterer

--- src/homework/02_expressions/main.cpp
+++ src/homework/02_expressions/main.cpp
@@ -16,7 +16,13 @@ int main()
 	double tip_amount;
 	double tax_amount;
 	double total;
-	cout << "Please enter the meal's cost, in dollars, before tax or tip";
+	cout << "Please enter the meal's cost, in dollars, before tax or tip\n";
 	cin >> meal_amount;
+	tax_amount = get_sales_tax_amount(meal_amount);
+	cout << "Please enter the tip rate\n";
+	cin >> tip_rate;
+	tip_amount = get_tip_amount(meal_amount, tip_rate);
+	total = tip_amount + tax_amount + meal_amount;
+	cout << "Meal Amount:	$" << meal_amount << "\nSales Tax:	$" << tax_amount << "\nTip Amount:	$" << tip_amount << "\nTotal:	$" << total;
 	return 0;
 }
