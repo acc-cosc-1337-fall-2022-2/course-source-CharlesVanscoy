31a2810c376d13dcd000be9b36783c1deb441863
CharlesVanscoy <charles.vanscoy@g.austincc.edu>
2022-09-17 23:56:53 -0500

    Working Better

--- src/homework/02_expressions/main.cpp
+++ src/homework/02_expressions/main.cpp
@@ -1,15 +1,22 @@
 ﻿//write include statements
-
-
+#include "hwexpressions.h"
+#include <iostream>
 //write namespace using statement for cout
-
+using std::cout;
+using std::cin;
 
 /*
 Call multiply_numbers with 10 and 10 parameter values and display function result
 */
+
 int main()
 {
-	
-
+	double meal_amount;
+	double tip_rate;
+	double tip_amount;
+	double tax_amount;
+	double total;
+	cout << "Please enter the meal's cost, in dollars, before tax or tip";
+	cin >> meal_amount;
 	return 0;
 }
