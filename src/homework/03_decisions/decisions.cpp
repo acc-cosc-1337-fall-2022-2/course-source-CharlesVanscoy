7a2a5b81c10bd8c306133b51dea20c06f51f4d99
CharlesVanscoy <charles.vanscoy@g.austincc.edu>
2022-09-24 23:18:58 -0500

    Hw_03 Finalmente

--- src/homework/03_decisions/decisions.cpp
+++ src/homework/03_decisions/decisions.cpp
@@ -1,4 +1,51 @@
 //write include statement for decisions header
-
+#include "decisions.h"
 
 //Write code for function(s) code here
+string get_letter_grade_using_if(int grade) {
+    string letter_grade;
+    if (grade <= 59 && grade >= 0) {
+        letter_grade = "F";
+    }
+    else if (grade <= 69 && grade >= 60) {
+        letter_grade = "D";
+    }
+    else if (grade <= 79 && grade >= 70) {
+        letter_grade = "C";
+    }
+    else if (grade <= 89 && grade >= 80) {
+        letter_grade = "B";
+    }
+    else if (grade <= 100 && grade >= 90) {
+        letter_grade = "A";
+    }
+    else {
+    cout << "Invalid input, number out of range.";    
+    }
+    return letter_grade;
+}
+
+string get_letter_grade_using_switch(int grade) {
+    string letter_grade;
+    switch (grade) {
+        case 0 ... 59: 
+            letter_grade = "F";
+            break;
+        case 60 ... 69: 
+            letter_grade = "D";
+            break;
+        case 70 ... 79: 
+            letter_grade = "C";
+            break;
+        case 80 ... 89: 
+            letter_grade = "B";
+            break;
+        case 90 ... 100: 
+            letter_grade = "A";
+            break;
+        default:
+            cout << "Invalid input, number out of range.";
+            break;
+    }
+    return letter_grade;
+}
--- src/homework/03_decisions/main.cpp
+++ src/homework/03_decisions/main.cpp
@@ -1,6 +1,26 @@
 //write include statements
+#include "decisions.h"
 
 int main() 
 {
-	return 0;
+	int choice;
+	int num_grade;
+	cout << "	MAIN MENU\n\n1-Letter grade using if\n2-Letter grade using switch\n3-Exit\n";
+	cin >> choice;
+	switch(choice)
+	{
+		case 1:
+			cout << "Enter numerical grade as an integer between 0 and 100\n";
+			cin >> num_grade;
+			cout << get_letter_grade_using_if(num_grade);
+			break;
+		case 2:
+			cout << "Enter numerical grade as an integer between 0 and 100\n";
+			cin >> num_grade;
+			cout << get_letter_grade_using_switch(num_grade);
+			break;
+		case 3:
+			break;
+
+	}
 }
