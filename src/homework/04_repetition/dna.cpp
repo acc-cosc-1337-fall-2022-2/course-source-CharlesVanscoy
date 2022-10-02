57da4a09ada07439532f8dc88abddf044bdd7a1d
CharlesVanscoy <charles.vanscoy@g.austincc.edu>
2022-10-01 23:55:59 -0500

    Program

--- src/homework/04_repetition/dna.cpp
+++ src/homework/04_repetition/dna.cpp
@@ -1,3 +1,43 @@
 //add include statements
+#include "dna.h"
 
+using std::cout;
+using std::cin;
+using std::string;
 //add function(s) code here
+
+int factorial(int num){
+    int i = num;
+    int product = 1;
+    while(i > 0){
+        product = i * product;
+        i--;
+    }
+    return product;
+}
+
+int gcd(int num1, int num2){
+    int temp;
+    while(num1 != num2){
+        if(num1 < num2){
+            temp = num1; num1 = num2; num2 = temp;
+        }   
+        else if(num1>num2){
+            num1 -= num2;
+        }
+    }
+    return num1;
+}
+
+int get_choice(){
+    string menu[] = {"Select the function you would like to use.\n","1-Factorial\n","2-Greatest Common Divisor\n","3-Exit\n"};
+	int choice;
+	int i = 0;
+
+	do {
+		cout << menu[i];
+		i++;
+		} while(i < 4);
+	cin >> choice;
+	return choice;
+}
--- src/homework/04_repetition/main.cpp
+++ src/homework/04_repetition/main.cpp
@@ -1,7 +1,9 @@
 //write include statements
-
+#include "dna.h"
 //write using statements
-
+using std::cout;
+using std::cin;
+using std::string;
 
 /*
 Write code that prompts user to enter 1 for Get GC Content, 
@@ -10,7 +12,43 @@ DNA string and call either get gc content or get dna complement
 function and display the result. Program runs as long as 
 user enters a y or Y.
 */
-int main() 
-{
+int main() {
+	bool finish = false;
+	int result = 0;
+	while(!finish) {
+		int num = 0;
+		int num1 = 0;
+		int num2 = 0;
+		int choice_main = get_choice();
+		char exit;
+		switch(choice_main) {
+			case 1:
+				cout << "What number would you like the factorial of?\n";
+				cin >> num;
+				result = factorial(num);
+				finish = true;
+				break;
+			case 2:
+				cout << "Enter two numbers to get their greatest common devisor\n";
+				cin >> num1;
+				cin >> num2;
+				result = gcd(num1,num2);
+				finish = true;
+				break;
+			case 3:
+				cout << "Are you sure you would like to exit? (y/n)\n";
+				cin >> exit;
+				if(exit == 'y'){
+					finish = true;
+				}
+				break;
+			default:
+				cout << "INVALID SELECTION PLEASE TRY AGAIN\n";
+				break;
+		}
+	}
+	if(result != 0){
+		cout << result;
+	}
 	return 0;
 }
