b92afc49c1d68f760d73808a39f2877b4ed68db2
CharlesVanscoy <charles.vanscoy@g.austincc.edu>
2022-10-08 23:57:22 -0500

    Hwk 05

--- src/homework/05_functions/func.cpp
+++ src/homework/05_functions/func.cpp
@@ -1,3 +1,50 @@
 //add include statements
-
+#include "func.h"
 //add function code here
+double get_gc_content(const string &dna){
+    double gc_cont = 0;
+    int i = dna.length();
+    int j = 0;
+    while(i > 0){
+        if(dna[i-1] == 'C' || dna[i-1] == 'G'){
+            gc_cont++;
+        }
+        i--;
+    }
+    return gc_cont/dna.length();
+}
+
+string get_dna_complement(string dna){
+    string dna_comp;
+    int i = 0;
+    while(i < dna.length()){
+        switch(dna[i]){
+            case 'G':
+                dna_comp += 'C';
+                break;
+            case 'C':
+                dna_comp += 'G';
+                break;
+            case 'A':
+                dna_comp += 'T';
+                break;
+            case 'T':
+                dna_comp += 'A';
+                break;
+        }
+        i++;
+    }
+    return dna_comp;
+}
+
+string reverse_string(string dna){
+    string revy;
+    int i = 0;
+    int j = dna.length() - 1;
+    while(i < dna.length()){
+        revy += dna[j];
+        i++;
+        j--;
+    }
+    return revy;
+}
